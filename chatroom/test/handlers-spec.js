dbaca2ea791c91f01342bd1730ddb94894aa8f66
Peter de Croos <pdecroos@sysomos.com>
2015-04-10 16:01:04 -0700

    added handlers-spec file with basic tests

---
+++ chatroom/test/handlers-spec.js
@@ -0,0 +1,56 @@
+var mocha = require('mocha');
+var should = require('should');
+
+
+describe('handlers', function() {
+  var handlers = require('../handlers.js');
+  /* basic requirements */
+
+  it('lets me sign up', function() {
+  
+  });
+
+  it('returns a jwt for loggin in', function() {
+  
+  });
+
+  it('errors out when given an invalid jwt', function() {
+  
+  });
+
+  it('lets me post a message', function() {
+  
+  });
+
+  it('gets back messages', function() {
+  
+  
+  });
+
+
+  /* advanced */
+
+  it('takes a timestamp and ony returns messages after that', function() {
+  
+  
+  });
+
+  
+
+  
+
+
+
+
+
+
+
+});
+
+
+
+
+
+
+
+

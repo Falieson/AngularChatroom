021d3a7c5e5dab1221b38bb5ae912e729c321da1
Peter de Croos <cultofmetatron@aumlogic.com>
2015-04-11 21:26:49 -0700

    added more objectives and removed the helper code

--- chatroom/handlers.js
+++ chatroom/handlers.js
@@ -9,65 +9,22 @@ var Users = new Store.UserCollection();
 
 module.exports.session = {
   login: function(req, res, next) {
-    var username = req.body.username;
-    var password = req.body.password;
-    Users.search(username, password)
-    .then(function(user) {
-      var token = jwt.sign({
-        username: user.username
-      }, config.secret);
-      //send the token back
-      res.send({
-        token: token
-      });
-    })
-    .catch(function(err) {
-      console.log('$$$', err)
-      next(err);
-    });
+      
   }
 };
 
 module.exports.user = {
   signup: function(req, res, next) {
-    console.log('hit')
-    var username = req.body.username;
-    var password = req.body.password;
-    return Users.signup(username, password)
-    .then(function(user) {
-      var token = jwt.sign({
-        username: user.username
-      }, config.secret);
-      //send the token back
-      res.send({
-        token: token
-      });
-    });
+  
   }
 };
 
 module.exports.chatMessages = {
   push: function(req, res, next) {
-    var username = req.user.username;
-    var message = req.body.message;
-    chatroom.pushMessage({
-      username: username,
-      message: message
-    })
-    .then(function() {
-      res.send({
-        status: 'success'
-      })
-    })
-    .catch(next);
+    
   },
   read: function(req, res, next) {
-    chatroom.retrieve().then(function(messages) {
-      res.send({
-        messages: messages
-      });
-    })
-    .catch(next);
+    
   },
 };
 

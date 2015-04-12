34b624349e7f85296814b9641102cca831079563
Peter de Croos <cultofmetatron@aumlogic.com>
2015-04-11 21:16:30 -0700

    stubbing out the controller

--- chatroom/mongoose-store/index.js
+++ chatroom/mongoose-store/index.js
@@ -6,15 +6,24 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var UserSchema = new Schema(require('./user-schema').schema);
 
-UserSchema.static('signup')
+UserSchema.static('signup', function(username, password) {
+  //code here
+});
+
+UserSchema.static('search', function(username, passsword) {
+
+});
 
 
+var MessageSchema = new Schema(require('./message-schema').schema);
 
+MessageSchema.static('pushMessage', function(msg) {
+  /* code here */
+});
 
-
-var messageSchema = new Schema(require('./message-schema').schema);
-
-
+MessageSchema.static('retrieve', function() {
+  /* code here */
+})
 
 
 

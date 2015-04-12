70a29644730e708e1f860d5ae02edd3e54e5b6af
Peter de Croos <cultofmetatron@aumlogic.com>
2015-04-12 14:12:27 -0700

    pushed promises info

--- scratch/promises/index.js
+++ scratch/promises/index.js
@@ -1,7 +1,7 @@
 var Promise = require('bluebird');
-var fs = require('fs');
+var fs = Promise.promisifyAll(require('fs'));
 var path = require('path');
-
+var _ = require('lodash')
 /*
 fs.readFile(path.join(__dirname, 'file1.txt'), 'utf8', function(err, res) {
   fs.readFile(path.join(__dirname, 'file2.txt'), 'utf8', function(err2, res2) {
@@ -11,5 +11,28 @@ fs.readFile(path.join(__dirname, 'file1.txt'), 'utf8', function(err, res) {
 */
 
 
+var files = ['file1.txt', 'file.txt'];
+
+Promise.all(_.map(files, function(filename) {
+  return fs.readFileAsync(filename, 'utf8')
+    .catch(function(err) {
+      //console.log('error logged', err)
+      throw new Error('arbitrary')
+    })
+
+}))
+.then(function(data) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      resolve(data[0] + data[1]);
+    }, 3000)
+  });
+})
+.then(function(contents) {
+  console.log(contents);
+})
+.catch(function(err) {
+  console.log('error logged', err)
+})
 
 

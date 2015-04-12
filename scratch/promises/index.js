53774297971c11422698471ff901c35fd6116bc2
Peter de Croos <cultofmetatron@aumlogic.com>
2015-04-12 13:56:02 -0700

    pushing more scratch stuff

---
+++ scratch/promises/index.js
@@ -0,0 +1,15 @@
+var Promise = require('bluebird');
+var fs = require('fs');
+var path = require('path');
+
+/*
+fs.readFile(path.join(__dirname, 'file1.txt'), 'utf8', function(err, res) {
+  fs.readFile(path.join(__dirname, 'file2.txt'), 'utf8', function(err2, res2) {
+    console.log(res + res2)
+  });
+});
+*/
+
+
+
+
